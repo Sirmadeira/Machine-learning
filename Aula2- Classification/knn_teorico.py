cfee04160e6cd4bf10efd1d50383eea62d1fdc25
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-06-07 11:42:11 -0300

    Aula2 finalizada

--- Aula2- Classification/knn_teorico.py
+++ Aula2- Classification/knn_teorico.py
@@ -22,32 +22,41 @@ def k_nearest_neighboor(data, predict, k=3):
 	votos=[i[1] for i in sorted(distancia) [:k]]
 	#print(Counter(votos).most_common(1))
 	votos_resultado= Counter(votos).most_common(1) [0] [0]
-	return votos_resultado
+	confianca=Counter(votos).most_common(1)[0][1] / k
+	return votos_resultado, confianca
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?',-99999, inplace=True)
-df.drop(['id'],1, inplace=True)
-full_data=df.astype(float).values.tolist()
-random.shuffle(full_data)
+precisoes=[]
 
-test_size= 0.2
-train_set ={2:[],4:[]}
-test_set ={2:[],4:[]}
-train_data=full_data[:-int(test_size*len(full_data))]
-test_data=full_data[-int(test_size*len(full_data)):]
+for i in range(5):
+	df = pd.read_csv('breast-cancer-wisconsin.data')
+	df.replace('?',-99999, inplace=True)
+	df.drop(['id'],1, inplace=True)
+	full_data=df.astype(float).values.tolist()
+	random.shuffle(full_data)
 
-for i in train_data:
-	train_set[i[-1]].append(i[:-1])
+	test_size= 0.2
+	train_set ={2:[],4:[]}
+	test_set ={2:[],4:[]}
+	train_data=full_data[:-int(test_size*len(full_data))]
+	test_data=full_data[-int(test_size*len(full_data)):]
 
-for i in test_data:
-	test_set[i[-1]].append(i[:-1])
+	for i in train_data:
+		train_set[i[-1]].append(i[:-1])
 
-correct=0
-total=0
-for group in test_set:
-	for data in test_set[group]:
-		voto= k_nearest_neighboor(train_set, data, k=5)
-		if group == voto:
-			correct+=1
-		total +=1
-print('Precisao', correct/total)
+	for i in test_data:
+		test_set[i[-1]].append(i[:-1])
+
+	correct=0
+	total=0
+	for group in test_set:
+		for data in test_set[group]:
+			voto,confianca= k_nearest_neighboor(train_set, data, k=5)
+			if group == voto:
+				correct+=1
+			#else:
+				#print(confianca)
+			total +=1
+	#print('Precisao', correct/total)
+	precisoes.append(correct/total)
+
+print(sum(precisoes)/len(precisoes))

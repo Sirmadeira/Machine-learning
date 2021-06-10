efc7deaf845b8f2689738b468eda3f575328baf6
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-06-10 16:20:30 -0300

    Tirando comentariso inuteis

--- Aula3-SVM/Svmtentativa2.py
+++ Aula3-SVM/Svmtentativa2.py
@@ -13,7 +13,6 @@ class Support_Vector_Machine:
     # train
     def fit(self, data):
         self.data = data
-        # { ||w||: [w,b] }
         opt_dict = {}
 
         transforms = [[1,1],
@@ -30,8 +29,6 @@ class Support_Vector_Machine:
         self.max_feature_value = max(all_data)
         self.min_feature_value = min(all_data)
         all_data = None
-
-        # support vectors yi(xi.w+b) = 1
         
 
         step_sizes = [self.max_feature_value * 0.1,
@@ -42,16 +39,14 @@ class Support_Vector_Machine:
 
         
         
-        # extremely expensive
         b_range_multiple = 2
-        # we dont need to take as small of steps
-        # with b as we do w
+
         b_multiple = 5
         latest_optimum = self.max_feature_value*10
         
         for step in step_sizes:
             w = np.array([latest_optimum,latest_optimum])
-            # we can do this because convex
+
             optimized = False
             while not optimized:
                 for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
@@ -60,17 +55,13 @@ class Support_Vector_Machine:
                     for transformation in transforms:
                         w_t = w*transformation
                         found_option = True
-                        # weakest link in the SVM fundamentally
-                        # SMO attempts to fix this a bit
-                        # yi(xi.w+b) >= 1
-                        # 
-                        # #### add a break here later..
+
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi=i
                                 if not yi*(np.dot(w_t,xi)+b) >= 1:
                                     found_option = False
-                                    #print(xi,':',yi*(np.dot(w_t,xi)+b))
+
                                     
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t,b]
@@ -103,11 +94,6 @@ class Support_Vector_Machine:
     def visualize(self):
         [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
 
-        # hyperplane = x.w+b
-        # v = x.w+b
-        # psv = 1
-        # nsv = -1
-        # dec = 0
         def hyperplane(x,w,b,v):
             return (-w[0]*x-b+v) / w[1]
 
@@ -115,20 +101,16 @@ class Support_Vector_Machine:
         hyp_x_min = datarange[0]
         hyp_x_max = datarange[1]
 
-        # (w.x+b) = 1
-        # positive support vector hyperplane
         psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
         psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
         self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2], 'k')
 
-        # (w.x+b) = -1
-        # negative support vector hyperplane
+
         nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
         nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
         self.ax.plot([hyp_x_min,hyp_x_max],[nsv1,nsv2], 'k')
 
-        # (w.x+b) = 0
-        # positive support vector hyperplane
+
         db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
         db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
         self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2], 'y--')
